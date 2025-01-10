07cbd813c3498bedc83b46928f2a0ee293ae7930
Matt <matthieu.chariercoquelin@gmail.com>
2025-01-10 15:40:56 +0100

    diazhidiaz

--- push_that.sh
+++ push_that.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-read -p "entrez un commit" cmsg
+read -p "entrez un commit " cmsg
+
+if [ "$cmsg" == "" ]; then
+  echo "MAIS IL FAUT UN MSG ENORME FDP GIGAFATASS"
+else 
+  echo ":)"
+fi
 
 git add . && git commit -m "$cmsg" && git push origin main 
+
 
